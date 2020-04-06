b517b7bd3ca009d2388503ed097c13040ca49255
Collin <dever.cj@gmail.com>
2020-04-06 00:31:45 -0700

    idirectionable implementation

--- TopTenList.cpp
+++ TopTenList.cpp
@@ -1,12 +1,27 @@
 //Manages a list of top ten hyperlinks
 #include <string>
+#include <iostream>
 #include "TopTenList.h"
 #include "Hyperlink.h"
 
 using std::string;
+using std::cout;
+using std::endl;
 
 TopTenList::TopTenList() {
     _list.resize(10);    
+}
+
+void TopTenList::display_forward() {
+    for(u_int i = 0; i < 10; i++) {
+        cout << i + 1 << ". <ahref=\"" <<_list[i].url << ">" << _list[i].text << "/a" << endl;
+    }
+}
+
+void TopTenList::display_backward() {
+        for(u_int i = 9; i >= 0; i--) {
+        cout << i + 1 << ". <ahref=\"" <<_list[i].url << ">" << _list[i].text << "/a" << endl;
+    }
 }
 
 void TopTenList::set_at(int index, Hyperlink link) {
